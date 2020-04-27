f07cd5befbaa2aad205eed63cedac0a9688294ff
Kuba Clark <jakub.clark@protonmail.com>
2020-04-27 02:56:27 +0200

    Move proto files

--- moosage-common/build.rs
+++ moosage-common/build.rs
@@ -2,6 +2,6 @@ fn main() {
     tonic_build::configure()
         .build_client(true)
         .build_server(true)
-        .compile(&["../proto/chat/chat_service.proto"], &["../proto"])
+        .compile(&["proto/chat/chat_service.proto"], &["proto"])
         .expect("Could not compile proto files");
 }
